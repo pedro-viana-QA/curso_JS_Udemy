a9e88d0762ca38f7c76a7a5dbcc774bb8eee1e77
pedro-viana-sete <pedro.viana@sete.inf.br>
2024-10-30 16:14:39 -0300

    Finalizando exercicio de estrutura

--- Ex_est_programação/Ex15.js
+++ Ex_est_programação/Ex15.js
@@ -1,11 +1,17 @@
-let contador
-let num = 6
-while(num > 0){
-    num / num
+let divisoes = 0
+let num = Math.floor(Math.random() * 100)
+console.log(num)
+let cont
+
+for(cont = 1; cont <= num; cont++){
+    if(num % cont == 0){
+        divisoes++
+        console.log(divisoes)
+    }
 }
 
-if(contador > 2){
-    console.log("O numero não é primo")
-}else{
+if(divisoes == 2){
     console.log("O numero é primo")
+}else{
+    console.log("O numero não é primo")
 }
